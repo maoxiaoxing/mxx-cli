4cb2f61a634408bc7821a61a9a7388b1b6604cb9
maoxiaoxing <930649054@qq.com>
2021-05-04 22:38:05 +0800

    命令行字体颜色

--- cli.js
+++ cli.js
@@ -10,12 +10,11 @@
 
 const inquirer = require("inquirer")
 const path = require('path')
-const fs = require('fs')
-const ejs = require('ejs')
 const program = require('commander')
 const writeFileUtil = require('./utils/writeFile')
 const download = require('download-git-repo')
 const ora = require('ora')
+const chalk = require('chalk')
 
 const templatesMap = new Map([
     ['vue-ts', {
@@ -85,11 +84,12 @@ program
         ])
         const { downloadUrl } = templatesMap.get(templateName)
 
-        const spinner = ora('正在下载中...')
+        const spinner = ora(chalk.blue('正在下载中...'))
         spinner.start()
 
         download(downloadUrl, projectName, { clone: true }, (err) => {
             if (err) {
+                spinner.text = chalk.red('初始化模板失败！')
                 spinner.fail()
                 return
             }
@@ -103,87 +103,9 @@ program
                     version,
                 }
             )
-            spinner.text = '初始化模板成功！'
+            spinner.text = chalk.green('初始化模板成功！')
             spinner.succeed()
         })
-        
-        return
-
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Project name?',
-                default: projectName
-            },
-            {
-                type: 'input',
-                name: 'version',
-                message: 'Project version?',
-                default: '1.0.0'
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Project description?',
-                default: ''
-            },
-        ])
-        .then(async (answers) => {
-            console.log(answers)
-            // 根据用户回答的结果生成文件
-
-            const { downloadUrl } = templatesMap.get(templateName)
-            console.log(downloadUrl)
-            
-
-
-            return
-            const tempHash = {
-                vue: 'templates/vue',
-                react: 'templates/react',
-            }
-            let tempPath
-            try {
-                tempPath = tempHash[templateName]
-                if (!tempPath) {
-                    throw new Error(`没有【 ${templateName} 】这个模板`)
-                }
-            }
-            catch(err) {
-                console.log(err)
-                process.exit() // 如果报错 终止进程
-            }
-
-            // 模板目录
-            const tmplDir = path.join(__dirname, tempPath)            
-
-            // 目标目录
-            const destDir = process.cwd()
-
-            // 将模板下的文件全部转换到目标目录
-            fs.readdir(tmplDir, (err, files) => {
-                if (err) throw err
-                files.forEach((file) => {
-                    console.log(file)
-                    // 通过模板引擎来渲染文件
-                    ejs.renderFile(path.join(tmplDir, file), answers, (err, res) => {
-                        if (err) throw err
-
-                        fs.writeFileSync(path.join(destDir, file), res)
-                        const packageJson = path.join(destDir, 'package.json')
-                        writeFileUtil.writeJson(
-                            packageJson, 
-                            {
-                                name: answers.name,
-                                version: answers.version
-                            }
-                        )                    
-                    })
-                })
-            })
-        })
-
     })
 
 program.parse(process.argv)
