76d0992ec877b832b2df132ab69c7d4b0bf47ecb
maoxiaoxing <930649054@qq.com>
2021-05-04 21:58:46 +0800

    写入package.json

--- cli.js
+++ cli.js
@@ -51,9 +51,7 @@ program
     .command('init')
     .description('初始化项目')
     .action(async () => {
-        // console.log(templateName, projectName)
         const templateNames = [...templatesMap.keys()]
-        console.log(templateNames)
 
         const { templateName } = await inquirer.prompt([
             {
@@ -64,7 +62,7 @@ program
             }
         ])
 
-        const { projectName } = await inquirer.prompt([
+        const { projectName, version } = await inquirer.prompt([
             {
                 type: 'input',
                 name: 'projectName',
@@ -88,11 +86,98 @@ program
 
         download(downloadUrl, projectName, { clone: true }, (err) => {
             if (err) {
-                console.log(err)
-            } else {
-                console.log('下载成功')
+                return console.log(err)
             }
+
+            const destDir = process.cwd()
+            const packageJson = path.join(`${destDir}/${projectName}`, 'package.json')
+            writeFileUtil.writeJson(
+                packageJson, 
+                {
+                    name: projectName,
+                    version,
+                }
+            )
+            console.log('初始化模板成功！')
         })
+        
+        return
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Project name?',
+                default: projectName
+            },
+            {
+                type: 'input',
+                name: 'version',
+                message: 'Project version?',
+                default: '1.0.0'
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Project description?',
+                default: ''
+            },
+        ])
+        .then(async (answers) => {
+            console.log(answers)
+            // 根据用户回答的结果生成文件
+
+            const { downloadUrl } = templatesMap.get(templateName)
+            console.log(downloadUrl)
+            
+
+
+            return
+            const tempHash = {
+                vue: 'templates/vue',
+                react: 'templates/react',
+            }
+            let tempPath
+            try {
+                tempPath = tempHash[templateName]
+                if (!tempPath) {
+                    throw new Error(`没有【 ${templateName} 】这个模板`)
+                }
+            }
+            catch(err) {
+                console.log(err)
+                process.exit() // 如果报错 终止进程
+            }
+
+            // 模板目录
+            const tmplDir = path.join(__dirname, tempPath)            
+
+            // 目标目录
+            const destDir = process.cwd()
+
+            // 将模板下的文件全部转换到目标目录
+            fs.readdir(tmplDir, (err, files) => {
+                if (err) throw err
+                files.forEach((file) => {
+                    console.log(file)
+                    // 通过模板引擎来渲染文件
+                    ejs.renderFile(path.join(tmplDir, file), answers, (err, res) => {
+                        if (err) throw err
+
+                        fs.writeFileSync(path.join(destDir, file), res)
+                        const packageJson = path.join(destDir, 'package.json')
+                        writeFileUtil.writeJson(
+                            packageJson, 
+                            {
+                                name: answers.name,
+                                version: answers.version
+                            }
+                        )                    
+                    })
+                })
+            })
+        })
+
     })
 
 program.parse(process.argv)
