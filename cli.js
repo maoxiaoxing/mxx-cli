3470ca7167e0784732c040c60a73f3ff02409132
maoxiaoxing <930649054@qq.com>
2021-06-20 19:58:33 +0800

    重构

--- cli.js
+++ cli.js
@@ -8,36 +8,13 @@
 // 1. 通过命令行交互询问用户问题
 // 2. 根据用户回答的结果生成文件
 
-const inquirer = require("inquirer")
 const path = require('path')
 const program = require('commander')
-const writeFileUtil = require('./utils/writeFile')
 const download = require('download-git-repo')
-const ora = require('ora')
-const chalk = require('chalk')
 const fs = require('fs')
-const os = require('os')
-const tools = require('./utils/tools')
+const { actionMap } = require('./utils/constants')
+const actionFn = require('./lib/index')
 
-const prefix = tools.isWindows() ? 'github' : 'https://github.com'
-
-const templatesMap = new Map([
-    ['vue-ts', {
-        url: 'https://github.com/maoxiaoxing/vue-ts-template',
-        downloadUrl: `${prefix}:maoxiaoxing/vue-ts-template#main`,
-        description: 'vue2 ts 项目模板',
-    }],
-    ['vue3', {
-        url: 'https://github.com/maoxiaoxing/vue3-study',
-        downloadUrl: `${prefix}:maoxiaoxing/vue3-study#master`,
-        description: 'vue3 + vue-cli 练习项目',
-    }],
-    ['react', {
-        url: '',
-        downloadUrl: '',
-        description: 'react',
-    }],
-])
 
 const pjsonPath = path.join(__dirname, 'package.json')
 const pjson = JSON.parse(fs.readFileSync(pjsonPath, 'utf-8'))
@@ -45,70 +22,16 @@ const pjson = JSON.parse(fs.readFileSync(pjsonPath, 'utf-8'))
 program
     .version(pjson.version, '-v, --version') // 输出版本号
 
-program
-    .command('list')
-    .description('查看所有模板')
-    .action(function() {
-        for(const [templateName, templateInfo] of templatesMap.entries()) {
-            const templateItem = chalk.gray(`${templateName} --- ${templateInfo.description}`)
-            console.log(templateItem)
-        }
-    })
-
-program
-    .command('init')
-    .description('初始化项目')
-    .action(async () => {
-        const templateNames = [...templatesMap.keys()]
-
-        const { templateName } = await inquirer.prompt([
-            {
-                type: 'list',
-                name: 'templateName',
-                message: '请选择一个模板',
-                choices: templateNames,
-            }
-        ])
-
-        const { projectName, version } = await inquirer.prompt([
-            {
-                type: 'input',
-                name: 'projectName',
-                message: '请为您的项目命名',
-                default: templateName,
-            },
-            {
-                type: 'input',
-                name: 'version',
-                message: 'Project version?',
-                default: '1.0.0'
-            },
-        ])
-        const { downloadUrl } = templatesMap.get(templateName)
-
-        const spinner = ora(chalk.blue('正在下载中...'))
-        spinner.start()
-
-        download(downloadUrl, projectName, { clone: true }, (err) => {
-            if (err) {
-                spinner.text = chalk.red('初始化模板失败！')
-                spinner.fail()
-                return
-            }
-
-            const destDir = process.cwd()
-            const packageJson = path.join(`${destDir}/${projectName}`, 'package.json')
-            writeFileUtil.writeJson(
-                packageJson, 
-                {
-                    name: projectName,
-                    version,
-                }
-            )
-            spinner.text = chalk.green('初始化模板成功！')
-            spinner.succeed()
+for (const [aName, aConfig] of actionMap.entries()) {
+    program
+        .command(aName)
+        .alias(aConfig.alias)
+        .description(aConfig.description)
+        .action(function() {
+            actionFn(aName, process.argv.slice(3))
         })
-    })
+}
+
 
 program.parse(process.argv)
 
--- utils/tools.js
+++ utils/tools.js
@@ -1,5 +1,6 @@
 const os = require('os')
 
+// 判断是否是 Windows 系统
 const isWindows = () => {
   const osType = os.type()
   return osType === 'Windows_NT'
