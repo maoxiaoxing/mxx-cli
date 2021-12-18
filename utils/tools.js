93ee75a03b8dde0f0c2f635cbc13890a151ba9fa
maoxiaoxing <930649054@qq.com>
2021-12-18 16:51:32 +0800

    工具函数

--- utils/tools.js
+++ utils/tools.js
@@ -8,6 +8,7 @@ const ejs = require('ejs')
 const writeFileAsync = util.promisify(fs.writeFile)
 const chalk = require('chalk')
 const { resolve } = require('path')
+const { rejects } = require('assert')
 
 // 判断是否是 Windows 系统
 const isWindows = () => {
@@ -59,5 +60,67 @@ const downloadDependencies = (templateName, dest, templatesMap) => {
   })
 }
 
+/**
+ * 将对象转换为 Map 数据结构
+ * @param {对象} obj 
+ * @returns 模板信息 Map
+ */
+const jsonToTemplateMap = (obj) => {
+  const strMap = new Map()
+  const prefix = 'https://github.com/'
+  for (const k of Object.keys(obj)) {
+    strMap.set(k, {
+      ...obj[k],
+      downloadUrl: `${prefix}maoxiaoxing/mxx-cli-templates#${k}`
+    })
+  }
+  return strMap
+}
+
+/**
+ * 获取模板信息
+ */
+const getTemplateMap = async () => {
+  const destDir = process.cwd()
+  await removeFile(`${destDir}/_template`)
+  const url = 'https://github.com/maoxiaoxing/mxx-cli-templates#master'
+  const readAsync = util.promisify(fs.readFile)
+  return new Promise((resolve, reject) => {
+    download(url, '_template', { clone: true }, async (err) => {
+      if (err) {
+        return reject(err)
+      }
+      const data = await readAsync(path.join(`${destDir}/_tempalte/template-config.json`))
+      const resultString = data.toString()
+      const resultJson = JSON.parse(resultString)
+      const tempaltesMap = jsonToTemplateMap(resultJson)
+      resolve(tempaltesMap)
+      await removeFile(`${destDir}/_tempalte`)
+    })
+  })
+}
+
+/**
+ * 渲染文件
+ * @param {文件} file 
+ * @param {模板内容} tempalteInfo 
+ * @returns 
+ */
+const renderFile = (file, tempalteInfo) => {
+  return new Promise((resolve, reject) => {
+    ejs.renderFile(file, tempalteInfo, async (err, data) => {
+      if (err) {
+        return resolve(chalk.red(`${file}写入失败！`))
+      }
+
+      await writeFileAsync(file, data)
+      resolve(chalk.green(`${file}写入成功！`))
+    })
+  })
+}
+
 exports.isWindows = isWindows
 exports.removeFile = removeFile
+exports.downloadDependencies = downloadDependencies
+exports.getTemplateMap = getTemplateMap
+exports.renderFile = renderFile
